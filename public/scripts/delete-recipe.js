61ceb165c1567e79beb1be2dd3d523b983121364
E3N2005 <ethanielkaim@gmail.com>
2026-02-20 14:49:32 -0500

    Changed success message

--- public/scripts/delete-recipe.js
+++ public/scripts/delete-recipe.js
@@ -8,3 +8,17 @@ deleteForms.forEach(button => {
         }
     });
 });
+
+// Auto-dismiss flash message
+const flash = document.querySelector('.flash-message');
+
+if (flash) {
+    setTimeout(() => {
+        flash.style.transition = "opacity 0.4s ease";
+        flash.style.opacity = "0";
+
+        setTimeout(() => {
+            flash.remove();
+        }, 400);
+    }, 3000);
+}
