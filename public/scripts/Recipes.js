9a3c0a2b6405ab08a77567cbcaf9a8bcf217a1c0
Mart2455 <Martin_m1717@icloud.com>
2026-02-25 15:17:12 -0500

    Added, a mechanism to pop up a recipe creation Overlay

--- public/scripts/Recipes.js
+++ public/scripts/Recipes.js
@@ -21,3 +21,113 @@ window.onclick = (e) => {
         FilterOverlay.style.display = "none";
     }
 }
+
+
+
+/* This script handles the opening and closing of the recipe creation modal on the recipes page. */
+function OpenRecipeCreationUI()
+{
+    document.getElementById("RecipeCreationUIPopUp").classList.add("active"); // Activate the Popup 
+}
+
+function CloseRecipeCreationUI()
+{
+    document.getElementById("RecipeCreationUIPopUp").classList.remove("active");
+}
+
+
+
+
+/* This script handles the dynamic addition and removal of Ingredients in the recipe creation form. */
+let Ingredients = [];
+
+function addIngredient()
+{
+    const Input = document.getElementById("IngredientInput");
+
+    const Value = Input.Value.trim();
+
+    if(Value === "")
+        return;
+
+    Ingredients.push(Value);
+
+    updateIngredientList();
+
+    Input.Value = "";
+}
+
+function updateIngredientList()
+{
+    const List = document.getElementById("IngredientList");
+
+    List.innerHTML = "";
+
+    Ingredients.forEach((Ingredient, index) =>
+    {
+        const NewList = document.createElement("NewList");
+
+        NewList.innerHTML =
+        Ingredient +
+        ' <button onclick="removeIngredient(' + index + ')">Remove</button>';
+
+        List.appendChild(NewList);
+    });
+
+    // Store as JSON string for backend
+    document.getElementById("IngredientsHidden").value =
+        JSON.stringify(Ingredients);
+}
+
+function removeIngredient(index)
+{
+    Ingredients.splice(index, 1);
+
+    updateIngredientList();
+}
+
+
+/* This script handles the dynamic addition and removal of Instructions in the recipe creation form. */
+
+
+let CurrentStep = 0;
+
+const Steps = document.querySelectorAll(".Step");
+
+function showStep(index)
+{
+    Steps.forEach(step => Step.classList.remove("active"));
+    Steps[index].classList.add("active");
+}
+
+function nextStep()
+{
+    const Inputs = Steps[CurrentStep].querySelectorAll("input, textarea");
+
+    for (let input of Inputs)
+    {
+        if (!input.value.trim())
+        {
+            alert("Please fill this field");
+            return;
+        }
+    }
+
+    CurrentStep++;
+
+    if (CurrentStep >= Steps.length)
+        CurrentStep = Steps.length - 1;
+
+    showStep(CurrentStep);
+}
+
+function prevStep()
+{
+    CurrentStep--;
+
+    if (CurrentStep < 0)
+        CurrentStep = 0;
+
+    showStep(CurrentStep);
+}
+
