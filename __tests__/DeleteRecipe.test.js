912959c958281e05bb1baa28fe3a1d73ff9393b4
Mart2455 <Martin_m1717@icloud.com>
2026-03-10 23:58:51 -0400

    Added the addrecipe unit test

---
+++ __tests__/DeleteRecipe.test.js
@@ -0,0 +1,20 @@
+
+/*
+ADD A RECIPE TO THE DATABASE
+SEARCHES THROUGH THE DATABASE TO FIND THE NEWLY CREATED RECIPE
+DELETES THE RECIPE
+*/ 
+
+
+const fs = require("fs");
+
+const path = require("path");
+
+const RECIPES_FILE_PATH = path.join(__dirname , "../data/recipes.json");
+
+describe("Delete Recipe ", () =>{
+
+    
+
+});
+
