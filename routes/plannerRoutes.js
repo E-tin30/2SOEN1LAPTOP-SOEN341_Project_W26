96f1668413b04b1fcf6513cf99b3e58baf825fd7
E3N2005 <ethanielkaim@gmail.com>
2026-03-16 17:23:23 -0400

    add meal-planner.ejs as template for meal-planner page and set up route for it

--- routes/plannerRoutes.js
+++ routes/plannerRoutes.js
@@ -3,3 +3,9 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const requireAuth = require('../middleware/requireAuth');
+
+router.get("/meal-planner", requireAuth, (req, res) => {
+    res.render('meal-planner', { title: 'Meal Planner', currentPage: 'planner', username: req.session.username });
+});
+
+module.exports = router;
