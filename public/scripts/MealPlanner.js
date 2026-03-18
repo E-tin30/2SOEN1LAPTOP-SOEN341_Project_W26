015a1bdd587df0b59b9213860270a8044f87bc78
kepler-paule <paulemilekepler@gmail.com>
2026-03-18 12:45:11 -0400

    Basic ui for adding recipe added

--- public/scripts/MealPlanner.js
+++ public/scripts/MealPlanner.js
@@ -1,3 +1,17 @@
 
+const addMealPlanForm = document.querySelector(".addMealPlanOverlay");
+const filterButton = document.getElementsByClassName("closeAddMealPlanOverlay");
+
+function toggleAddMealPlanOverlay(action) 
+{
+
+    if (action === "open") {
+        addMealPlanForm.style.display = "flex";
+    } else if (action === "close") {
+        addMealPlanForm.style.display = "none";
+    }
+}
+
+
 
 
