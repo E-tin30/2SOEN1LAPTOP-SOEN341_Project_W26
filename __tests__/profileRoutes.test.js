06de2c1f9e22bc84cd8223d5b1e3e8cb7ce12a5b
E3N2005 <ethanielkaim@gmail.com>
2026-03-18 22:23:03 -0400

    added profile test

--- __tests__/profileRoutes.test.js
+++ __tests__/profileRoutes.test.js
@@ -7,3 +7,9 @@ const RECIPES_FILE_PATH = path.join(__dirname, "../data/recipes.json");
 const USERS_FILE_PATH = path.join(__dirname, "../data/users.json");
 const PREFERENCES_FILE_PATH = path.join(__dirname, "../data/preferences.json");
 
+describe("Profile Routes", () => {
+    test("GET /profile redirects if not logged in", async () => {
+        const res = await request(app).get("/profile");
+        expect(res.statusCode).toBe(302);
+    });
+});
